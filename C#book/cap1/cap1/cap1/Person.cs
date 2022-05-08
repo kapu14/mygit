5162bdd14e51cb9894a19afd427b4147672ddb96
n-maeda <n-maeda@media-inc.co.jp>
2022-05-08 20:19:14 +0900

    cap1完了

--- C#book/cap1/cap1/cap1/Person.cs
+++ C#book/cap1/cap1/cap1/Person.cs
@@ -8,11 +8,17 @@ namespace cap1
     {
         public string Name { get; set; }
         
-        public DateTime BirthDay { get; set; }
+        public DateTime Birthday { get; set; }
 
         public int GetAge()
         {
-
+            DateTime today = DateTime.Today;
+            int age = today.Year - Birthday.Year;
+            if(today <Birthday.AddYears(age))
+            {
+                age--;
+            }
+            return age;
         }
     }
 }
--- C#book/cap1/cap1/cap1/Program.cs
+++ C#book/cap1/cap1/cap1/Program.cs
@@ -12,7 +12,7 @@ namespace cap1
             var dorayaki = new Product(98, "どら焼き", 210);
 
             Console.WriteLine(dorayaki.GetTax());
-            Console.ReadLine();
+            //Console.ReadLine();
 
 
             //問題１．２
@@ -27,7 +27,32 @@ namespace cap1
             PrintObjects(a, b);
             Console.WriteLine(a.X + " " + a.Y);
             Console.WriteLine(b.X + " " + b.Y);
+            //Console.ReadLine();
+
+
+            var yamada = new Student
+            {
+                Name = "Yamada",
+                Birthday = new DateTime(1972, 07, 14),
+                Grade = 1,
+                ClassNumber = 2,
+
+            };
+
+            var c = new object();
+            c = yamada;
+
+            var d = new Person();
+            d = yamada;
+
+            object e = yamada;
+            Person f = yamada;
+
+
+
+            Console.WriteLine(yamada.Name + " " + yamada.Birthday + " " + yamada.Grade + " " + yamada.ClassNumber);
             Console.ReadLine();
+
         }
 
         internal static void PrintObjects(MyClass myclass,MyStruct mystruct)
